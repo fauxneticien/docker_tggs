e1594f7a2f11f25e6114d9d1f18b76e025f1f693
Nay San <nay.san@anu.edu.au>
2017-12-05 16:54:24 +1100

    Added run.R into image

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM rocker/tidyverse
 RUN apt-get update && apt-get install -y libv8-dev && \
     Rscript -e 'devtools::install_github("fauxneticien/phonpack")' && \
     Rscript -e 'install.packages("googlesheets")'
+
+ADD run.R /
---
+++ run.R
@@ -0,0 +1,49 @@
+library(phonpack)
+library(base64enc)
+library(googlesheets)
+
+# Get from BitBucket pipeline environment
+BATCH_NO <- Sys.getenv("BITBUCKET_REPO_SLUG")
+AUTH_STR <- Sys.getenv("GOOGLE_AUTH")
+
+# Convert env variable to txt file, then to RDS file, then supply
+# to gs_auth() as RDS file
+base64_token <- tempfile()
+decoded_rds  <- tempfile()
+writeLines(text = AUTH_STR, con = base64_token)
+base64decode(file = base64_token, output = decoded_rds)
+
+auth_token  <- gs_auth(token = decoded_rds, cache = FALSE)
+kdict_trans <- gs_title("kdict-transcriptions")
+
+# Create new worksheet if not already present
+if(!BATCH_NO %in% gs_ws_ls(kdict_trans)) {
+    gs_ws_new(kdict_trans, ws_title = BATCH_NO)
+}
+
+# List all TextGrids
+list.files(
+    path    = ".",
+    pattern = ".TextGrid$",
+    recursive = TRUE
+) %>%
+
+# Read 'ipa' tier from all listed TextGrids
+map_df(function(tg_filename) {
+    
+    tg_filename %>%
+    readTextGrid() %>%
+    getTierByName("ipa") %>%
+    getTierIntervals(discard_empty = TRUE) %>%
+    mutate(source_file = tg_filename)        
+
+}) %>% 
+
+# Overwrite sheet with new data
+gs_edit_cells(
+    ss        = kdict_trans,
+    ws        = BATCH_NO,
+    input     = .,
+    anchor    = "A1",
+    col_names = TRUE
+)
